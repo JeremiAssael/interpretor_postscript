e2edb9664c6fb43bc8caa94917f7f051236facf1
Jeremi Assael <36697022+JeremiAssael@users.noreply.github.com>
2018-04-11 02:22:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,32 @@ from class_token import *
 from class_useful_objects import *
 
 
-
-"""def inter(instruction, dico_function):
+def inter(instruction, dico_function):
     instr=Instruction(instruction)
     tab_instruction=instr.split_instruction()
-    n=len(tab_instruction)
     pile=Stack([])
     L=[]
-    for i in range(n):
-        tab_instruction[i]=get_type(tab_instruction[i])
+    for i in range(len(tab_instruction)):
+        if tab_instruction[i]!="{" and tab_instruction[i]!="}":
+            tab_instruction[i]=get_type(tab_instruction[i])
+    """tous les éléments de notre instruction, quels qu'ils soient ont été transtypés"""
+    tab_instruction=split_tot(tab_instruction)   
+    tab_instruction=conv_procedure(tab_instruction)
+    for i in range(len(tab_instruction)):
         pile, dico_function=tab_instruction[i].operation(pile, i, dico_function)
+        print(pile.liste)
     L.append(pile)
     L.append(dico_function)
     return L
+
+
+def conv_procedure(liste):
+    for i in range(len(liste)):
+        if type(liste[i]) is list:
+            print(liste[i])
+            conv_procedure(liste[i])
+            liste[i]=Procedure(liste[i])
+    return liste
 
 
 
@@ -27,48 +40,7 @@ while instruction!="exit":
     print(liste[0].print_stack())
     dico_function=liste[1]
     print(dico_function)
-    instruction=input(">>>")"""
-
-
-def inter(instruction, pile, dico_function):
-    instr=Instruction(instruction)
-    tab_instruction=instr.split_instruction()
-    n=len(tab_instruction)
-    L=[]
-    for i in range(n):
-        tab_instruction[i]=get_type(tab_instruction[i])
-        pile, dico_function=tab_instruction[i].operation(pile, i, dico_function)
-    L.append(pile)
-    L.append(dico_function)
-    return L
-
-
-
-dico_function={}
-instruction=input(">>>")
-liste=[]
-pile=Stack([])
-while instruction!="exit":
-    liste=inter(instruction, pile, dico_function)
-    print(liste[0].print_stack())
-    pile=liste[0]
-    dico_function=liste[1]
-    print(dico_function)
     instruction=input(">>>")
 
 
-
-"""dico_function={}
-instruction=input(">>>")
-liste=[]
-pile=[]
-while instruction!="exit":
-    try:
-        liste=inter(instruction, dico_function)
-        print(liste[0].print_stack())
-        dico_function=liste[1]
-        print(dico_function)
-        instruction=input(">>>")
-    except:
-        instruction=input(">>>")"""
 
