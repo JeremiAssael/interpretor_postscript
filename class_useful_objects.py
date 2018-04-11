e27f32858f34ecba887260b9cbf482b2ba9f1ad6
Jeremi Assael <36697022+JeremiAssael@users.noreply.github.com>
2018-04-11 02:21:41 +0200

    Update class_useful_objects.py

--- class_useful_objects.py
+++ class_useful_objects.py
@@ -2,6 +2,7 @@
 
 import random
 import string
+import re
 
 class Stack():
     """Classe des piles: utilisées lors de l'interrétation de l'instruction entrée par l'utilisateur, utilisée pour renvoyer le résultat"""
@@ -15,9 +16,9 @@ class Stack():
             """Si mon stack est vide, on affiche une chaine de caractère vide"""
             string=""
         else:
-            string=str(self.liste[0])
+            string=str(self.liste[0].string)
             for i in range(1,len(self.liste)):
-                string+=" " + str(self.liste[i])
+                string+=" " + str(self.liste[i].string)
         return string
 
     def append(self, element):
@@ -64,34 +65,44 @@ class Instruction():
     def __init__(self, instruction):
         self.instruction=instruction
 
+
     def split_instruction(self):
         """Méthode permettant de séparer ma chaîne de carcatère instruction en tokens (qui sont aussi des chaînes de caractères)"""
-        size=len(self.instruction)
-        comp_acc=0
-        L=[]
-        replacement=[]
-        maillon_tab=[]
-        for i in range(size):
+        nb=self.instruction.count("{")
+        for i in range(len(self.instruction)+nb):
             if self.instruction[i]=="{":
-                L.append(i)
-                for j in range(i+1,size):
-                    if self.instruction[j]=="{":
+                self.instruction=self.instruction[0:i+1]+" "+self.instruction[i+1:len(self.instruction)+1]
+        nb2=self.instruction.count("(\S)?}")
+        for i in range(len(self.instruction)+nb2):
+            print(self.instruction)
+            print(i)
+            print(len(self.instruction))
+            if self.instruction[i]=="}" and not re.match(" ",self.instruction[i-1]) and not re.match("{",self.instruction[i-2]):
+                self.instruction=self.instruction[0:i]+" "+self.instruction[i:len(self.instruction)+1]
+            elif self.instruction[i]=="}" and re.match(" ",self.instruction[i-1]) and re.match("{",self.instruction[i-2]):
+                self.instruction=self.instruction[0:i]+" "+self.instruction[i:len(self.instruction)+1]
+        instruction_split=self.instruction.split()
+        return instruction_split
+
+def split_tot(liste):
+    while liste.count("{")!=0:
+        n=len(liste)
+        K=[]
+        comp_acc=0
+        for i in range(n):
+            if liste[i]=="{":
+                K.append(i)
+                for j in range(i+1,n):
+                    if liste[j]=="{":
                         comp_acc+=1
-                    elif self.instruction[j]=="}":
+                    elif liste[j]=="}":
                         comp_acc=comp_acc-1
                         if comp_acc==-1:
-                            L.append(j)
-                            maillon=self.instruction[L[0]:L[1]+1]
-                            maillon_tab.append(maillon)
-                            rand=''.join([random.choice(string.ascii_letters + string.digits) for n in range(L[1]+1-L[0])])
-                            """verifier que rand n'est pas dans le dico des fonctions ou un nom dans le reste de la chaine, sinon change"""
-                            self.instruction=self.instruction.replace(maillon,rand)
-                            replacement.append(rand)
-                            L=[]
-                            comp_acc=0
-        string_split_no_proc=self.instruction.split()
-        for i in range(len(replacement)):
-            for j in range(len(string_split_no_proc)):
-                if string_split_no_proc[j]==replacement[i]:
-                    string_split_no_proc[j]=maillon_tab[i]
-        return string_split_no_proc
+                            K.append(j)
+        liste_procedure=liste[K[0]+1:K[1]]
+        liste_procedure=split_tot(liste_procedure)
+        for l in range(K[0], K[1]+1):
+            liste.pop(K[0])
+        liste.insert(K[0], liste_procedure)
+    return liste    
+
